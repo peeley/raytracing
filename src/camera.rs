d788d6cf8d68d69aaccfae8676cc194cc988af93
Noah Snelson <noah.snelson@protonmail.com>
2020-07-13 16:33:18 -0700

    Rustfmt Camera::new()

--- src/camera.rs
+++ src/camera.rs
@@ -9,7 +9,13 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(look_from: Coordinate, look_at: Coordinate, vup: Coordinate, vfov: f32, aspect_ratio: f32) -> Self {
+    pub fn new(
+        look_from: Coordinate,
+        look_at: Coordinate,
+        vup: Coordinate,
+        vfov: f32,
+        aspect_ratio: f32,
+    ) -> Self {
         let theta = vfov.to_radians();
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
@@ -22,10 +28,7 @@ impl Camera {
         let origin = look_from;
         let horizontal = viewport_width * u;
         let vertical = viewport_height * v;
-        let lower_left_corner = origin
-            - (horizontal / 2.0)
-            - (vertical / 2.0)
-            - w;
+        let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - w;
 
         Camera {
             origin,
